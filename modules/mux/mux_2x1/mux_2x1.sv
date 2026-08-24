f5582355bdc19d648c3a3b379b8a42530ef72c38
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-08-24 15:43:27 -0700

    Create mux_2x1.sv

---
+++ modules/mux/mux_2x1/mux_2x1.sv
@@ -0,0 +1,16 @@
+`timescale 1ns / 1ps
+
+module mux_2x1(
+    input logic x0,
+    input logic x1,
+    input logic s,
+    output logic f
+    );
+    
+    logic p0, p1;
+    
+    assign f = p0 | p1;
+    assign p0 = x0 & ~s; 
+    assign p1 = x1 & s;
+    
+endmodule
