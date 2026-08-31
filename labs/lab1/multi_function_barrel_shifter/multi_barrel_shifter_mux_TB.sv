3d6d5d3dc92198e30c58a4853270feb9efe020b6
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-08-31 14:59:28 -0700

    Update multi_barrel_shifter_mux_TB.sv

--- labs/lab1/multi_function_barrel_shifter/multi_barrel_shifter_mux_TB.sv
+++ labs/lab1/multi_function_barrel_shifter/multi_barrel_shifter_mux_TB.sv
@@ -2,23 +2,25 @@
 
 module barrel_shifter_mux_TB;
 
-logic [2**N - 1:0] in,
-logic lr, // left-right select line (left = 0, right = 1)
-logic [N-1:0] amt,
-logic [2**N - 1:0] out,
+parameter N = 3;
+logic [2**N - 1:0] in;
+logic lr; // left-right select line (left = 0, right = 1)
+logic [N - 1:0] amt;
+logic [2**N - 1:0] out;
 
-multi_barrel_shifter_mux #(.N(2)) UUT (.in(in), .lr(lr), .amt(amt), .out(out));
+multi_barrel_shifter_mux #(.N(N)) UUT (.in(in), .lr(lr), .amt(amt), .out(out));
 
-initial begin
-    in = 4'b1011;
+    initial begin
+        in = 8'b1101;
+        
+        // left shift by 2
+        amt = 3'b10;
+        lr = 1'b0;
     
-    // left shift by 2
-    amt = 2'b10;
-    lr = 1'b0;
-
-    #5 // right shift by 2
-    lr = 1'b1;
-
-    #5
-    $stop;
-end
+        #5 // right shift by 2
+        lr = 1'b1;
+    
+        #5
+        $stop;
+    end
+endmodule
