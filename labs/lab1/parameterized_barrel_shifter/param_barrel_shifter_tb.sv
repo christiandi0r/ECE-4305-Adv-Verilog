99b7c522cd7bdd6b0783faf94f6c874d9c6948ce
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-08-31 14:57:05 -0700

    Update param_barrel_shifter_tb.sv

--- labs/lab1/parameterized_barrel_shifter/param_barrel_shifter_tb.sv
+++ labs/lab1/parameterized_barrel_shifter/param_barrel_shifter_tb.sv
@@ -2,22 +2,23 @@
 
 module param_barrel_shifter_tb;
 
-logic [3:0] r_in, r_out;
-logic [1:0] r_amount;
+parameter N = 3;
+logic [2**N - 1:0] r_in, r_out;
+logic [N - 1:0] r_amount;
 
-logic [3:0] l_in, l_out;
-logic [1:0] l_amount;
+logic [2**N - 1:0] l_in, l_out;
+logic [N - 1:0] l_amount;
 
-param_right_shifter #(.N(2)) uut (.in(r_in), .amount(r_amount), .out(r_out));
-param_left_shifter #(.N(2)) uut2 (.in(l_in), .amount(l_amount), .out(l_out));
+param_right_shifter #(.N(N)) uut (.in(r_in), .amount(r_amount), .out(r_out));
+param_left_shifter #(.N(N)) uut2 (.in(l_in), .amount(l_amount), .out(l_out));
 
 initial 
     begin
-        r_in = 4'b1011;
-        r_amount = 2'b10;
+        r_in = 8'b1011_1111;
+        r_amount = 3'b110;
         
-        l_in = 4'b1101;
-        l_amount = 2'b10;
+        l_in = 8'b1111_1101;
+        l_amount = 3'b110;
         
         #100
         $stop;
