e8245b0458b43637802a9f1238d27248222b5c14
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-08-31 14:58:31 -0700

    Update param_mux_2x1.sv

--- labs/lab1/multi_function_barrel_shifter/param_mux_2x1.sv
+++ labs/lab1/multi_function_barrel_shifter/param_mux_2x1.sv
@@ -1,11 +1,11 @@
 `timescale 1ns / 1ps
 
 module param_mux_2x1
-#(parameter N)
+#(parameter N = 2)
 (
-    input  logic [N-1:0] in0, in1,
+    input  logic [2**N - 1:0] in0, in1,
     input  logic sel,
-    output logic [N-1:0] f
+    output logic [2**N - 1:0] f
 );
 
 assign f = sel ? in1 : in0;
