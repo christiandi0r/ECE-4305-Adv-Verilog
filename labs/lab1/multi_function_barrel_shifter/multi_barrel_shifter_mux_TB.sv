83d644ef36cec7c58cb331a90a0c85aadb05df67
Lyrux <jonathankwong15@gmail.com>
2026-08-31 13:02:50 -0700

    added testbench for multi function barrel shifter mux

---
+++ labs/lab1/multi_function_barrel_shifter/multi_barrel_shifter_mux_TB.sv
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+
+module barrel_shifter_mux_TB;
+
+logic [2**N - 1:0] in,
+logic lr, // left-right select line (left = 0, right = 1)
+logic [N-1:0] amt,
+logic [2**N - 1:0] out,
+
+multi_barrel_shifter_mux #(.N(2)) UUT (.in(in), .lr(lr), .amt(amt), .out(out));
+
+initial begin
+    in = 4'b1011;
+    
+    // left shift by 2
+    amt = 2'b10;
+    lr = 1'b0;
+
+    #5 // right shift by 2
+    lr = 1'b1;
+
+    #5
+    $stop;
+end
