f4366601ff72ddc3aed82c730a88b98c4c28e8e4
Lyrux <jonathankwong15@gmail.com>
2026-08-31 12:51:53 -0700

    Implemented multi_barrel_shifter_mux.sv

---
+++ labs/lab1/multi_function_barrel_shifter/multi_barrel_shifter_mux.sv
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+
+module multi_barrel_shifter_mux
+#(parameter N)
+(
+    input logic [2**N - 1:0] in,
+    input logic lr, // left-right select line (left = 0, right = 1)
+    input logic [N-1:0] amt,
+    input logic [2**N - 1:0] out,
+);
+
+logic [2**N - 1:0] left_out, right_out;
+
+param_left_shifter left_shift
+(
+    .in(in),
+    .amount(amt), 
+    .out(left_out)
+);
+
+param_right_shifter right_shift
+(
+    .in(in),
+    .amount(amt), 
+    .out(right_out)
+);
+
+param_mux_2x1 M0
+(
+    .in0(left_out),
+    .in1(right_out),
+
+    // lr = left-right select line (left = 0, right = 1)
+    .sel(lr),
+    .f(out)
+);
--- labs/lab1/multi_function_barrel_shifter/param_mux_2x1.sv
+++ labs/lab1/multi_function_barrel_shifter/param_mux_2x1.sv
@@ -4,12 +4,10 @@ module param_mux_2x1
 #(parameter N)
 (
     input  logic [N-1:0] in0, in1,
-
-    // lr = left-right select line (left = 0, right = 1)
-    input  logic lr,
+    input  logic sel,
     output logic [N-1:0] f
 );
 
-assign f = lr ? in1 : in0;
+assign f = sel ? in1 : in0;
 
 endmodule
