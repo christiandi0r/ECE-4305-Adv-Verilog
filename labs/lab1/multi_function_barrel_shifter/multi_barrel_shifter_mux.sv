d4ae6875e9b058e77089b8160a78a8938bc292ff
Christian Ruelas <109180938+christiandi0r@users.noreply.github.com>
2026-08-31 14:58:56 -0700

    Update multi_barrel_shifter_mux.sv

--- labs/lab1/multi_function_barrel_shifter/multi_barrel_shifter_mux.sv
+++ labs/lab1/multi_function_barrel_shifter/multi_barrel_shifter_mux.sv
@@ -1,31 +1,31 @@
 `timescale 1ns / 1ps
 
 module multi_barrel_shifter_mux
-#(parameter N)
+#(parameter N = 2)
 (
     input logic [2**N - 1:0] in,
     input logic lr, // left-right select line (left = 0, right = 1)
     input logic [N-1:0] amt,
-    input logic [2**N - 1:0] out
+    output logic [2**N - 1:0] out
 );
 
 logic [2**N - 1:0] left_out, right_out;
 
-param_left_shifter left_shift
+param_left_shifter #(.N(N)) left_shift 
 (
     .in(in),
     .amount(amt), 
     .out(left_out)
 );
 
-param_right_shifter right_shift
+param_right_shifter #(.N(N)) right_shift
 (
     .in(in),
     .amount(amt), 
     .out(right_out)
 );
 
-param_mux_2x1 M0
+param_mux_2x1 #(.N(N)) M0
 (
     .in0(left_out),
     .in1(right_out),
@@ -34,4 +34,5 @@ param_mux_2x1 M0
     .sel(lr),
     .f(out)
 );
+
 endmodule
